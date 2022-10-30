0ef4b9ab1583cc5ce5ba64eac254072d091edc9b
Derek Yeh <derek.yeh91@gmail.com>
2022-10-30 14:37:50 -0700

    updated CSS

--- main.js
+++ main.js
@@ -1,94 +1,68 @@
 // queryselectors and variables HERE
 var form = document.querySelector('form');
 var mealTypes = document.getElementsByName('foodOption')
-var letsCookButton = document.querySelector('.letsCook');
-var clearButton = document.querySelector('.clear');
+var letsCookButton = document.querySelector('#letsCook');
+var clearButton = document.querySelector('.clearContainer');
 var cookpotImg = document.querySelector('.cookpot-img');
-
-// var for the image container to change the innertext
+var prompt1 = document.querySelector('.prompt');
+var suggestionPlaceholder = document.querySelector('.suggestion-placeholder');
 var box2 = document.querySelector('.box2');
 
-var sides = ['Mashed Potatoes', 'French Fries', 'Chips and Salsa', 
-'Sweet Potato Fries', 'Hush Puppies', 'Garlic Butter Mushrooms', 
-'Mixed Vegetables', 'Grilled Asparagus',
-'Potato Salad', 'Caesar Salad', 'Coleslaw']
-var mainDishes = ['Lobster Mac and Cheese', 'Teriyaki Chicken', 
-'Pepperoni Pizza', 'Bibimbap', 'Shashuka', 'Taiwanese Beef Noodle Soup',
-'Spaghetti and Meatballs', 'Sushi', 'Corned Beef Hash']
-var desserts = ['Tiramisu', 'Apple Pie', 'Macaroons', 'Banana Bread',
-'Strawberry Cheesecake', 'Pavlova', 'Croissants', 'Pumpkin Pie', 
-'Snickerdoodle Cookies']
+var sides = ['Mashed Potatoes', 'French Fries', 'Chips and Salsa', 'Sweet Potato Fries', 'Hush Puppies', 'Garlic Butter Mushrooms', 'Mixed Vegetables', 'Grilled Asparagus','Potato Salad', 'Caesar Salad', 'Coleslaw']
+var mainDishes = ['Lobster Mac and Cheese', 'Teriyaki Chicken','Pepperoni Pizza', 'Bibimbap', 'Shashuka', 'Taiwanese Beef Noodle Soup', 'Spaghetti and Meatballs', 'Sushi', 'Corned Beef Hash']
+var desserts = ['Tiramisu', 'Apple Pie', 'Macaroons', 'Banana Bread','Strawberry Cheesecake', 'Pavlova', 'Croissants', 'Pumpkin Pie', 'Snickerdoodle Cookies']
+
 var entireMeal = `${mainDishes[Math.floor(Math.random()*mainDishes.length)]} with ${sides[Math.floor(Math.random()*sides.length)]} and ${desserts[Math.floor(Math.random()*desserts.length)]} for dessert!`
 
-
 // event listeners HERE
-form.addEventListener('submit', testFunction)
+letsCookButton.addEventListener('click', function(){
+    letsCookDisplays();
+    showSuggestion();
+})
 clearButton.addEventListener('click', function(){
     goToHomePage();
-    clearSuggestion();
-    console.log("testing")
 })
 
 // functionality and other behavior HERE
 
-function testFunction(event) {
-    box2.innerHTML = "";
-    var element = document.querySelector('form');
+function showSuggestion() {
+    var foodOptions = document.getElementsByName('foodOption');
 
-    for(i = 0; i < element.length; i++) {
-        if(element[i].checked) {
-            var value = element[i].value;
-            // console.log(value)
+    for(i = 0; i < foodOptions.length; i++) {
+        if(foodOptions[i].checked) {
+            var checkedChoice = foodOptions[i].value;
         }
     }
-    switch(value) {
+    switch(checkedChoice) {
         case "Side":
-            box2.innerHTML = `
-            <section class="foodSuggestion">
-                <div class="prompt">You should make:</div>
-                <BR>
-                <div id="randomSuggestion">${sides[Math.floor(Math.random()*sides.length)]}</div>
-                <button class="clearButton">Clear</button>
-            </section>` 
-          break;
+            suggestionPlaceholder.innerText = sides[Math.floor(Math.random()*sides.length)]+'!';
+            break;
         case "Main Dish":
-            box2.innerHTML = `
-            <section class="foodSuggestion">
-                <div class="prompt">You should make:</div>
-                <BR>
-                <div id="randomSuggestion">${sides[Math.floor(Math.random()*mainDishes.length)]}</div>
-            
-            </section>`
-          break;
+            suggestionPlaceholder.innerText = mainDishes[Math.floor(Math.random()*mainDishes.length)]+'!';
+            break;
         case "Dessert":
-            box2.innerHTML = `
-            <section class="foodSuggestion">
-                <div class="prompt">You should make:</div>
-                <BR>
-                <div id="randomSuggestion">${sides[Math.floor(Math.random()*desserts.length)]}</div>
-            </section>` 
+            suggestionPlaceholder.innerText = desserts[Math.floor(Math.random()*desserts.length)]+'!';
             break;
         case "Entire Meal":
-            box2.innerHTML = `
-            <section class="foodSuggestion">
-                <div class="prompt">You should make:</div>
-                <BR>
-                <div id="randomSuggestion">${entireMeal}</div>
-            </section>` 
+            suggestionPlaceholder.innerHTML = entireMeal+'!';
             break;
         default:
             break;
     }
+}
+
+function letsCookDisplays(){
     event.preventDefault();
+    cookpotImg.classList.add('hidden');
+    suggestionPlaceholder.classList.remove('hidden');
+    prompt1.classList.remove('hidden');
+    clearButton.classList.remove('hidden');
 }
 
 function goToHomePage (){
-    cookpotImg.classList.add('hidden')
-    mealTypes.classList.add('hidden')
+    cookpotImg.classList.remove('hidden');
+    clearButton.classList.add('hidden');
+    suggestionPlaceholder.classList.add('hidden');
+    prompt1.classList.add('hidden');
 }
 
-function clearSuggestion() {
-    console.log("function is being called")
-    box2.innerHTML = ""
-    box2.innerHTML = `<img class="cookpot-img" src="./assets/cookpot.svg" alt="broken img">`
-}
